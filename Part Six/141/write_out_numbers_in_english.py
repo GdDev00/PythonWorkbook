3d089a95629944a32e235134cff574e69c8f29fa
GdDev00 <giacomo.dellomo00@gmail.com>
2020-10-26 22:13:42 +0100

    Edited

--- Part Six/141/write_out_numbers_in_english.py
+++ Part Six/141/write_out_numbers_in_english.py
@@ -10,32 +10,38 @@ dict2 = {"10":"ten","11":"eleven","12":"twelve", "13":"thirteen", "14":"fourteen
 #dictionary for multiples of 10
 dict3 = {"2":"twenty","3":"thirty","4":"fourty","5":"fifty","6":"sixty","7":"seventy","8":"eighty","9":"ninety"}
 
-number= input("Inserisci un numero: ")
-result = ""
-lenght = len(number)
-
-while lenght>0:
-    if lenght==1:
-        if number[0] in dict1:
-            result += dict1[number[0]] + " "           
-        number = number[1:]
-
-    elif lenght==2:
-        if int(number)<20:
-            if number in dict2.keys():
-                result += dict2[number] + " "
-        else:
-            if number[0] in dict3.keys():
-                result += dict3[number[0]] + " "
-        number = number[1:]
-
-    elif lenght==3:
-        result += dict1[number[0]] + " hundred "
-        number = number[1:]
-
-    else:
-        print("Numero inserito non valido!")
-
+def number_in_english(number):
+    result = ""
     lenght = len(number)
 
+
+    while lenght>0:
+        if lenght==1:
+            if number[0] in dict1:
+                result += dict1[number[0]] + " "           
+            number = number[1:]
+
+        elif lenght==2:
+            if int(number)<20:
+                if number in dict2.keys():
+                    result += dict2[number] + " "
+            else:
+                if number[0] in dict3.keys():
+                    result += dict3[number[0]] + " "
+            number = number[1:]
+
+        elif lenght==3:
+            result += dict1[number[0]] + " hundred "
+            number = number[1:]
+
+        else:
+            print("Numero inserito non valido!")
+
+        lenght = len(number)
+
+    return result
+
+
+number= input("Inserisci un numero: ")
+result = number_in_english(number)
 print(result)
