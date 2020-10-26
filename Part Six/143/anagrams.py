df78022a3e893c2a0a86f0dfd421a53cb2233bb7
GdDev00 <giacomo.dellomo00@gmail.com>
2020-10-26 22:26:31 +0100

    Add ex. 143

---
+++ Part Six/143/anagrams.py
@@ -0,0 +1,24 @@
+#Ex. 143 Anagrams
+
+def char_count(word):
+    chars_dict = dict()
+
+    for char in word:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            chars_dict[char] = 1
+    
+    return chars_dict
+
+    
+word1 = input("Inserisci una parola: ")
+word2 = input("Insersci la parola da confrontare: ")
+
+counts1 = char_count(word1)
+counts2 = char_count(word2)
+
+if counts1 == counts2:
+    print("Le parole sono anagrammi!")
+else:
+    print("Le parole non sono anagrammi!")
