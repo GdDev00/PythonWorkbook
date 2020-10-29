9f199e1d1291e4ded32183dcee85a5fa177e7300
GdDev00 <giacomo.dellomo00@gmail.com>
2020-10-29 08:37:55 +0100

    Completed

--- Part Eight/175/recurisve_decimal_to_binary.py
+++ Part Eight/175/recurisve_decimal_to_binary.py
@@ -1,6 +1,6 @@
 #Ex. 175 Recursive Decimal To Binary
 
-def decimal_to_binary(number, binary):
+def decimal_to_binary(number, binary=""):
     #base case
     if number == 0:
         return binary
@@ -10,4 +10,12 @@ def decimal_to_binary(number, binary):
         binary = str(number%2) + binary
         return decimal_to_binary(number//2,binary)
 
-print(decimal_to_binary(150,""))
+def main():
+    print("Recursive Decimal to Binary converter")
+    num = int(input("Please, insert a non negative number-->\n"))
+    if num>0:
+        print("The binary number is: "+  decimal_to_binary(num))
+    else:
+        print("The number must be a non negative integer!")
+
+main()
