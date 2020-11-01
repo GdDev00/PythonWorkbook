d35578d3c865d19e08702cc4ed8501e85c466a36
GdDev00 <giacomo.dellomo00@gmail.com>
2020-11-01 12:06:19 +0100

    Add exercise

--- Part Eight/186/compress_encoded_list.py
+++ Part Eight/186/compress_encoded_list.py
@@ -1,4 +1,24 @@
 #Ex. 186
 
-def compress(string):
+def compress(data):
+    #base case
+    if len(data)==0:
+        return []
+
+    index = 1
+    while index < len(data) and data[index] == data[index-1]:
+        index += 1
+        
+    compressed = [data[0], index]
+
+    return compressed + compress(data[index : len(data)])
+
+def main():
+    # Read a string from the user
+    s = input("Enter some characters: ")
+
+    # Display the result
+    print("When those characters are run-length encoded," "the result is:", compress(s))
     
+main()    
+
